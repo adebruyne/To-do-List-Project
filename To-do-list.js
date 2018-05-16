253c1284566097815e4c55674a1265bf08946881
Aylin DeBruyne <adebruyne88@gmail.com>
2018-05-16 15:20:26 -0400

    make simple todo list

---
+++ To-do-list.js
@@ -0,0 +1,23 @@
+var todos = [];
+
+var intput = prompt("What would you like to do?")
+
+
+
+
+
+
+while(input !=="quit"){
+    //handle input
+    if(input === "list") {
+        console.log(todos);
+    } else if (input === "new"){
+        //ask for new todo
+        var newTodo = prompt("Enter new todo");
+        //add to todos array
+        todos.push(newTodo);
+    }
+    //ask again for new input
+    intput = prompt("What would you like to do?")
+}
+console.log("OK, You quit the app")
